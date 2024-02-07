ca58827bbd7c812de95b472fdf81e8c689613c52
svante2001 <svante2001@gmail.com>
2024-02-07 16:06:04 +0100

    Store n and fib(n) in hashmap

--- Fibonacci/unikernel.ml
+++ Fibonacci/unikernel.ml
@@ -1,10 +1,13 @@
 open Lwt.Infix
 
+let fibs : (int, int) Hashtbl.t = Hashtbl.create 10
+
 let rec fib n =
   if n < 3 then 1
   else fib (n - 1) + fib (n - 2)
 
 module Main (S : Tcpip.Stack.V4V6) = struct
+  let r = ref 0
   let start s =
     let port = Key_gen.port () in
     S.TCP.listen (S.tcp s) ~port (fun flow ->
@@ -25,7 +28,15 @@ module Main (S : Tcpip.Stack.V4V6) = struct
             Logs.debug (fun f ->
                 f "read: %d bytes:\n%s" (Cstruct.length b) (Cstruct.to_string b));
 
-            S.TCP.write flow (Cstruct.of_string (Printf.sprintf "Fib(%s) = %d" (Cstruct.to_string b) (fib (int_of_string (Cstruct.to_string b))))) >>= function
+            (
+              if Hashtbl.mem fibs (int_of_string (Cstruct.to_string b))
+              then r := Hashtbl.find fibs (int_of_string (Cstruct.to_string b))
+              else 
+                r := (fib (int_of_string (Cstruct.to_string b)));
+                Hashtbl.add fibs (int_of_string (Cstruct.to_string b)) !r
+            );
+
+            S.TCP.write flow (Cstruct.of_string (Printf.sprintf "%d" !r)) >>= function
 
             | _ -> S.TCP.close flow); 
     S.listen s
