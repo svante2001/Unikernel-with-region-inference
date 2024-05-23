66d203cc94a23a0e3db1f1a2d8cc40430b47df84
svante2001 <svante2001@gmail.com>
2024-05-23 15:30:39 +0200

    Fragmentation test import

---
+++ tests/UDPfragmentation.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Make echo
+make echo-app
+
+# Run echo
+nohup ./echo.exe 2>&1 &
+echo $! > PID.txt
+
+# Send Hamlet.txt
+echo Sending file
+cat tests/hamlet.txt | nc -u -nw1 10.0.0.2 8080 > tests/out.txt
+
+# Close echo
+kill $(cat PID.txt)
+
+# Compare
+if diff tests/hamlet.txt tests/out.txt > /dev/null; then
+    echo "Files are the same."
+else
+    echo "Error: Files are different!"
+    exit 1
+fi
+
+# Clean
+rm PID.txt
+rm nohup.out
+rm tests/out.txt
