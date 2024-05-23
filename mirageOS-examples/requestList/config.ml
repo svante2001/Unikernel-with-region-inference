645734a2efd325e2fdc19298dc8766f2c7f9e48b
svante2001 <svante2001@gmail.com>
2024-05-23 11:33:05 +0200

    rm unwanted files

--- mirageOS-examples/requestList/config.ml
+++
@@ -1,8 +0,0 @@
-open Mirage
-
-let main =
-  main ~packages:[ package "cohttp-mirage" ] "Unikernel.Main" (conduit @-> job)
-
-let () =
-  register "conduit_server"
-    [ main $ conduit_direct (generic_stackv4v6 default_network) ]
